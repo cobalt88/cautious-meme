d4ffdbb19ae395caabde660e3d4a8ad062a48215
cobalt88 <vincentteune@gmail.com>
2022-05-03 23:58:02 -0400

    5.3.22
    playing with middlewares. L

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require('http');
-const express = requre('express')
+const express = require('express')
 const app = express()
-const server = http.createServer();
+const server = http.createServer(app);
 
+
+app.use((req, res, next) => {
+  console.log('in the middleware');
+  next(); //allows the request to continue ti the next middleware in line
+
+});
+
+app.use((req, res, next) => {
+  console.log('in the middleware 2')
+})
 server.listen(3001);
