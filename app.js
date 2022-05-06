9eb631e025d9241add90e2130d5ed7e0ed7f250f
cobalt88 <vincentteune@gmail.com>
2022-05-06 19:15:51 -0400

    5.6.22
    add in static file route to public folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 
