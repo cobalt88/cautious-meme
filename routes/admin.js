18cc7207a15e5d69bed8f5755f53d427e0272480
cobalt88 <vincentteune@gmail.com>
2022-05-07 00:08:57 -0400

    5.7.22
     beginning of module 6, generatin gdynamic content

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const rootDir = require('../util/path')
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
@@ -10,8 +11,9 @@ res.sendFile(path.join(rootDir, 'views', 'admin.html'));
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+products.push({ title: req.body.title });
+res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
