047c6bb8404c4be7eefebe92a61b8beb9ff03b79
cobalt88 <vincentteune@gmail.com>
2022-05-20 16:03:26 -0400

    5.20.22
    rename hadlebars file name extensions and make modifications to app.js:

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-app.engine('handlebars', expressHbs());
-app.set('view engine', 'handlebars');
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(PORT);
