86f7c375fb472c816c26dd96b0fcdb758d5a0178
cobalt88 <vincentteune@gmail.com>
2022-05-20 15:44:59 -0400

    5.20.22
    build templated html pages with pug and handlebars

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const path = require('path');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+app.engine('handlebars', expressHbs());
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(PORT);
