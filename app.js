42d21b99ecedbba96a9fb1e55cd1240e09de86e5
cobalt88 <vincentteune@gmail.com>
2022-05-22 20:51:03 -0400

    5.22.22
    break up routes into controllers

--- app.js
+++ app.js
@@ -3,26 +3,24 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(PORT);
 console.log(`Listening on PORT: ${PORT}`)
--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,12 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
-const rootDir = require('../util/path');
-const adminData = require('./admin');
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+const express = require('express');
+
+const productController = require('../controllers/products');
+
+const router = express.Router();
+
+
+router.get("/", productController.getProducts);
 
 module.exports = router
